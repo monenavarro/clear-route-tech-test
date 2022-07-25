f36afff47eda264821333b48c9e6a8f6e2669b5d
Monica Navarro <me.navarro.97@outlook.com>
2022-07-25 11:22:14 +0100

    Code is able to read .txt file

---
+++ stage-1/filter_search.py
@@ -0,0 +1,11 @@
+# Search criteria:
+# Customers aged 40-59
+# Name, phone number and email address
+
+# Python to read .txt file
+with open('stage-1/latest-customers.txt') as f:
+  contents = f.read()
+  print(contents)
+
+
+# Python to write new .txt file using the search criteria
