4df46556decf6028c623b772bfef78fadc9c7dfd
Monica Navarro <me.navarro.97@outlook.com>
2022-07-26 12:05:42 +0100

    Code successfully produces new .txt file with the affected customers details

--- stage-1/filter_search.py
+++ stage-1/filter_search.py
@@ -2,15 +2,31 @@
 # Customers aged 40-59
 # Name, phone number and email address
 
-# Python to read .txt file
+# I am going to convert the data from the .txt file to csv
+# There are python functions that work well with csv from my experience
 
-# x = open("stage-1/latest-customers.txt")
-# for line in x:
-#   if line[1] == '50':
-#     print(line)
-# print("Customers aged 40")
+import csv
 
-# Now that the program is able to read the .txt file, I now need to write a function that filters the customers age
+with open("stage-1/latest-customers-converted.csv", mode = 'r') as file:
+  # reading csv file
+  csvFile = csv.reader(file)
 
-import pandas
+  # creating empty strings to store data from csv
+  age = []
+  name = []
+  phone_number = []
+  email = []
 
+  # creating a for loop to iterate through lines in csv file
+  for lines in csvFile:
+    age = lines[1]
+    name = lines[0]
+    phone_number = lines[3]
+    email = lines[4]
+    if age >= '40' and age <= '59':
+      str = age + " " + name + " " + phone_number + " " + email + "\n"
+      
+      # write data into .txt file
+      f = open("affected-customers.txt", "a")
+      f.write(str)
+      f.close()
